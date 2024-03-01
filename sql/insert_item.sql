33335fb4a20bde9f7c0cd502b9bcc3646c25052e
Tyson <henry@henrys-mbp.home>
2024-03-01 15:57:20 -0500

    initial commit langchain usecases

---
+++ sql/insert_item.sql
@@ -0,0 +1,8 @@
+INSERT INTO item(item_id, item_name, item_price)
+VALUES 
+		(1, "Teddy Bear", 12.99),
+		(2, "Chess Board", 24.99),
+		(3, "Soccer Ball", 14.87),
+		(4, "Doll", 9.99),
+		(5, "Xbox", 499.99);
+		
